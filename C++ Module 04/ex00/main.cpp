68e4b8fd1cd9afe09c85a70a957dcd8ef19d533f
sdk-meb <use2ess@yahoo.com>
2022-11-19 18:31:03 +0100

    cpp-04/ex01  Approximately

--- C++ Module 04/ex00/main.cpp
+++ C++ Module 04/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mes-sadk <mes-sadk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sdk-meb <sdk-meb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:22:40 by mes-sadk          #+#    #+#             */
-/*   Updated: 2022/11/19 12:55:46 by mes-sadk         ###   ########.fr       */
+/*   Updated: 2022/11/19 17:18:29 by sdk-meb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@ int main(){
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat;
-    // Dog p;
-    // Cat pd(p);
 
-    // p = pd;
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     
+
     i->makeSound(); //will output the cat sound!
     j->makeSound();
     meta->makeSound();
+    
     delete j;
     delete i;
     delete meta;
